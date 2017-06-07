5b5816c7b4df2e053d066438dfd8a1c060632e79
buobao <320683buobao>
2017-06-07 22:35:57 +0800

    push

--- app/src/main/java/com/king/turman/downloadingbutton/MainActivity.java
+++ app/src/main/java/com/king/turman/downloadingbutton/MainActivity.java
@@ -1,7 +1,14 @@
 package com.king.turman.downloadingbutton;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.king.turman.library.DownloadButton;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final double[] currentPercent = {0.0};
+        final DownloadButton db = (DownloadButton) findViewById(R.id.loading_btn);
+        final Handler handler = new Handler(){
+            @Override
+            public void handleMessage(Message msg) {
+                db.setCurrentPercent(currentPercent[0]);
+            }
+        };
+
+        Timer timer = new Timer(true);
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                currentPercent[0] += 0.05;
+                handler.sendEmptyMessage(0);
+            }
+        };
+
+        timer.schedule(timerTask,500,200);
     }
 }
--- library/src/main/java/com/king/turman/library/DownloadButton.java
+++ library/src/main/java/com/king/turman/library/DownloadButton.java
@@ -4,10 +4,14 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.Toast;
+
+import java.util.Timer;
 
 /**
  * Created by diaoqf on 2017/6/7.
@@ -23,6 +27,13 @@ public class DownloadButton extends View {
     private int loadingArcColor;
     private float loadingArcWidth;
 
+    private int centerBtnColor;
+    private float centerBtnWidth;
+
+    private boolean isLoading;
+
+    private double currentPercent;
+
 
     public DownloadButton(Context context) {
         super(context);
@@ -36,6 +47,8 @@ public class DownloadButton extends View {
 
     private void init(Context context, @Nullable AttributeSet attrs){
         mPaint = new Paint();
+        isLoading = false;
+        currentPercent = 0;
         mPaint.setAntiAlias(true);
         TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.DownloadButton);
         baseArcColor = a.getColor(R.styleable.DownloadButton_base_arc_color,getResources().getColor(R.color.base_arc_color));
@@ -43,7 +56,21 @@ public class DownloadButton extends View {
 
         loadingArcColor = a.getColor(R.styleable.DownloadButton_loading_arc_color,getResources().getColor(R.color.load_arc_color));
         loadingArcWidth = a.getDimension(R.styleable.DownloadButton_loading_arc_width,2);
+
+        centerBtnColor = a.getColor(R.styleable.DownloadButton_center_btn_color,getResources().getColor(R.color.base_arc_color));
+        centerBtnWidth = a.getDimension(R.styleable.DownloadButton_center_btn_width,-1);
         a.recycle();
+
+        setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isLoading) {
+                    isLoading = false;
+                } else {
+                    isLoading = true;
+                }
+            }
+        });
     }
 
     @Override
@@ -61,7 +88,50 @@ public class DownloadButton extends View {
 
         mPaint.setColor(loadingArcColor);
         mPaint.setStrokeWidth(loadingArcWidth);
-        canvas.drawArc(new RectF(loadingArcWidth/2,loadingArcWidth/2,getMeasuredWidth()-loadingArcWidth/2,getMeasuredHeight()-loadingArcWidth/2),-90,90,false,mPaint);
+        canvas.drawArc(new RectF(loadingArcWidth/2,loadingArcWidth/2,getMeasuredWidth()-loadingArcWidth/2,getMeasuredHeight()-loadingArcWidth/2),-90, (float) (360 * currentPercent),false,mPaint);
 
+        if (centerBtnWidth < 0) {
+            centerBtnWidth = getMeasuredWidth() / 3;
+        }
+
+        mPaint.setColor(centerBtnColor);
+        mPaint.setStyle(Paint.Style.FILL);
+        if (isLoading) {
+            canvas.drawRect(new RectF(getMeasuredWidth() / 2 - centerBtnWidth / 2,
+                    getMeasuredHeight() / 2 - centerBtnWidth / 2,
+                    getMeasuredWidth() / 2 + centerBtnWidth / 2,
+                    getMeasuredWidth() / 2 + centerBtnWidth / 2), mPaint);
+        } else {
+            Path path = new Path();
+            path.moveTo((float) (getMeasuredWidth()/2 -  centerBtnWidth*Math.sqrt(3)/8), getMeasuredHeight()/2 - centerBtnWidth/2);
+            path.lineTo((float) (getMeasuredWidth()/2 + centerBtnWidth*Math.sqrt(3)*3/8), getMeasuredHeight()/2);
+            path.lineTo((float) (getMeasuredWidth()/2 -  centerBtnWidth*Math.sqrt(3)/8), getMeasuredHeight()/2 + centerBtnWidth/2);
+            canvas.drawPath(path, mPaint);
+        }
+
+    }
+
+
+    public boolean isLoading() {
+        return isLoading;
+    }
+
+    public boolean isLoadingFinished() {
+        if (currentPercent >= 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void setCurrentPercent(double currentPercent) {
+        if (isLoading) {
+            this.currentPercent = currentPercent;
+            invalidate();
+        }
+
+        if (currentPercent >= 1.0) {
+            isLoading = false;
+        }
     }
 }
